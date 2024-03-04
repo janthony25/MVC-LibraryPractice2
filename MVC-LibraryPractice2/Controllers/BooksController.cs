546174f9a2022000501b9d2c172f3c2472f9167f
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-04 21:27:29 +1300

    Ad author to Edit, details and delete

--- MVC-LibraryPractice2/Controllers/BooksController.cs
+++ MVC-LibraryPractice2/Controllers/BooksController.cs
@@ -56,7 +56,7 @@ namespace MVC_LibraryPractice2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BookId,Title,CallNumber")] Book book)
+        public async Task<IActionResult> Create([Bind("BookId,Title,CallNumber, Author")] Book book)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace MVC_LibraryPractice2.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("BookId,Title,CallNumber")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("BookId,Title,CallNumber, Author")] Book book)
         {
             if (id != book.BookId)
             {
