398343840d40e1a1085f71ebe9c2fca7e019e2f6
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-04 21:22:42 +1300

    Add author to index

--- MVC-LibraryPractice2/Migrations/20240304082152_AddedAuthor.Designer.cs
+++ MVC-LibraryPractice2/Migrations/20240304082152_AddedAuthor.Designer.cs
@@ -3,6 +3,7 @@ using MVC_LibraryPractice2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_LibraryPractice2.Migrations
 {
     [DbContext(typeof(LibraryPractice2Context))]
-    partial class LibraryPractice2ContextModelSnapshot : ModelSnapshot
+    [Migration("20240304082152_AddedAuthor")]
+    partial class AddedAuthor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
