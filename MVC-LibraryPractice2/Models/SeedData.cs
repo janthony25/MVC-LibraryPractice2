22a335121d800a7b55e3b34ce80a37bf35cbf5a6
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-04 21:14:15 +1300

    Add seedData

---
+++ MVC-LibraryPractice2/Models/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_LibraryPractice2.Data;
+
+namespace MVC_LibraryPractice2.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryPractice2Context(
+                    serviceProvider.GetRequiredService<
+                    DbContextOptions<LibraryPractice2Context>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+
+                context.Book.AddRange(
+                    new Book { Title = "Mini dot-net project", CallNumber = "ASD 142"},
+                    new Book { Title = "test1", CallNumber = "TRY 145"}
+                    );
+            }
+        }
+    }
+}
