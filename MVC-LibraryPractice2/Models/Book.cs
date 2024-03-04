d692459008024de02b0da892b5fd1eaa22bbbf00
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-04 21:03:50 +1300

    Add book model

---
+++ MVC-LibraryPractice2/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_LibraryPractice2.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string CallNumber { get; set; }
+
+    }
+}
